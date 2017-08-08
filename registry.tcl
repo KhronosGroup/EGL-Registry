238c451f66dba0f7a4f518e405c678df55bd5ec9
Adam Jackson <ajax@redhat.com>
2017-08-08 12:53:29 -0400

    extensions: Add EGL_EXT_explicit_device
    Signed-off-by: Adam Jackson <ajax@redhat.com>

--- registry.tcl
+++ registry.tcl
@@ -762,4 +762,9 @@ extension EGL_EXT_surface_compression {
     flags       public
     filename    extensions/EXT/EGL_EXT_surface_compression.txt
 }
-# Next free extension number: 148
+extension EGL_EXT_explicit_device {
+    number      148
+    flags       public
+    filename    extensions/EXT/EGL_EXT_explicit_device.txt
+}
+# Next free extension number: 149
