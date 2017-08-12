153d0b347600c6e4a3e45316b4228f2db6d74fed
Nicolas Capens <capn@google.com>
2017-08-11 23:54:27 -0400

    Add EGL platform typedefs for Ozone.
    Ozone is a platform abstraction layer used by Chromium:
    https://chromium.googlesource.com/chromium/src/+/master/docs/ozone_overview.md

--- api/EGL/eglplatform.h
+++ api/EGL/eglplatform.h
@@ -92,6 +92,13 @@ typedef struct ANativeWindow*           EGLNativeWindowType;
 typedef struct egl_native_pixmap_t*     EGLNativePixmapType;
 typedef void*                           EGLNativeDisplayType;
 
+#elif defined(USE_OZONE)
+
+/* Chromium-specific */
+typedef intptr_t EGLNativeDisplayType;
+typedef intptr_t EGLNativeWindowType;
+typedef intptr_t EGLNativePixmapType;
+
 #elif defined(__unix__)
 
 /* X11 (tentative)  */
