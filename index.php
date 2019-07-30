37aeee8dbcc2ddb9c8c7325deb81576fcb1be46a
santanu <santanu-thangaraj@users.noreply.github.com>
2019-07-30 15:20:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -337,6 +337,8 @@ include_once("../../assets/static_pages/khr_page_top.php");
 </li>
 <li value=134> <a href="extensions/NV/EGL_NV_stream_origin.txt">EGL_NV_stream_origin</a>
 </li>
+<li value=135> <a href="extensions/NV/EGL_NV_stream_dma.txt">EGL_NV_stream_dma</a>
+</li>
 </ol>
 
 <h6> Providing Feedback on the Registry </h6>
