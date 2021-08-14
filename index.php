12d8663bc71687f9c8bb6e3c7c7cd460cba1b98e
Jon Leech <oddhack@sonic.net>
2021-08-14 07:38:59 -0700

    Update index.php

--- index.php
+++ index.php
@@ -355,6 +355,8 @@ include_once("../../assets/static_pages/khr_page_top.php");
 </li>
 <li value=144> <a href="extensions/EXT/EGL_EXT_device_drm_render_node.txt">EGL_EXT_device_drm_render_node</a>
 </li>
+<li value=145> <a href="extensions/EXT/EGL_EXT_config_select_group.txt">EGL_EXT_config_select_group</a>
+</li>
 </ol>
 
 <h6> Providing Feedback on the Registry </h6>
--- registry.tcl
+++ registry.tcl
@@ -1,4 +1,4 @@
-# Copyright 2006-2020 The Khronos Group Inc.
+# Copyright 2006-2021 The Khronos Group Inc.
 # SPDX-License-Identifier: Apache-2.0
 
 # registry.tcl
