7dc46bcb983f6553235b4ae00381a4fefc0a1e72
maniraj87 <89836548+maniraj87@users.noreply.github.com>
2021-10-02 17:23:41 +0530

    Reserve enum for EGL_OPENWF_DEVICE_EXT (#134)
    With revision #5 of EGL_EXT_device_openwf extension, EGL_OPENWF_DEVICE_EXT
    attribute was added to the spec but it was somehow not added in egl.xml,
    adding it now.

--- api/EGL/egl.h
+++ api/EGL/egl.h
@@ -14,7 +14,7 @@ extern "C" {
 ** used to make the header, and the header can be found at
 **   http://www.khronos.org/registry/egl
 **
-** Khronos $Git commit SHA1: 9581d004ff $ on $Git commit date: 2021-04-06 15:53:59 +0200 $
+** Khronos $Git commit SHA1: b35e89ca9a $ on $Git commit date: 2021-09-01 09:34:00 +0530 $
 */
 
 #include <EGL/eglplatform.h>
@@ -23,7 +23,7 @@ extern "C" {
 #define EGL_EGL_PROTOTYPES 1
 #endif
 
-/* Generated on date 20210802 */
+/* Generated on date 20210901 */
 
 /* Generated C header for:
  * API: egl
