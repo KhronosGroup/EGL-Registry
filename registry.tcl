cb629b25987516cbb67b623862efe9899addbeef
Jeff Vigil <j.vigil@samsung.com>
2023-06-15 13:21:15 -0600

    Fix case and next available extension number

--- registry.tcl
+++ registry.tcl
@@ -777,4 +777,4 @@ extension EGL_QNX_platform_screen {
     flags       public
     filename    extensions/QNX/EGL_QNX_platform_screen.txt
 }
-# Next free extension number: 150
+# Next free extension number: 151
