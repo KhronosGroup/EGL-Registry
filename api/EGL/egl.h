682c662d48fbae076c5ed89a1bd5b2aa7e2e4449
Tom Murphy <tomnomnom1@gmail.com>
2024-07-16 00:36:36 +0000

    extensions: Add EGL_ANDROID_telemetry_hint (#204)
    * extensions: Add EGL_ANDROID_telemetry_hint
    
    Add a new EGL_TELEMETRY_HINT_ANDROID to help Android telemetry disguise
    between OpenGL contexts.
    
    * extensions: Add EGL_ANDROID_telemetry_hint
    
    Add a new EGL_TELEMETRY_HINT_ANDROID to help Android telemetry disguise
    between OpenGL contexts.
    
    * fix range and position of added xml
    
    * Change EGL_ANDROID_TELEMETRY_HINT to EGL_TELEMETRY_HINT_ANDROID to match other android enums
    
    * Add EGL-Registry pull #204 in the range comments
    
    * Fix txt file path
    
    Co-authored-by: janharaldfredriksen-arm <jan-harald.fredriksen@arm.com>
    
    ---------
    
    Co-authored-by: tomnom <tomnom@google.com>
    Co-authored-by: janharaldfredriksen-arm <jan-harald.fredriksen@arm.com>

--- api/EGL/egl.h
+++ api/EGL/egl.h
@@ -14,7 +14,7 @@ extern "C" {
 ** used to make the header, and the header can be found at
 **   http://www.khronos.org/registry/egl
 **
-** Khronos $Git commit SHA1: f4cc936b88 $ on $Git commit date: 2023-12-16 01:21:49 -0500 $
+** Khronos $Git commit SHA1: 402ce7f591 $ on $Git commit date: 2024-07-09 16:33:12 +0000 $
 */
 
 #include <EGL/eglplatform.h>
@@ -23,7 +23,7 @@ extern "C" {
 #define EGL_EGL_PROTOTYPES 1
 #endif
 
-/* Generated on date 20231215 */
+/* Generated on date 20240710 */
 
 /* Generated C header for:
  * API: egl
--- api/EGL/eglext.h
+++ api/EGL/eglext.h
@@ -14,12 +14,12 @@ extern "C" {
 ** used to make the header, and the header can be found at
 **   http://www.khronos.org/registry/egl
 **
-** Khronos $Git commit SHA1: f4cc936b88 $ on $Git commit date: 2023-12-16 01:21:49 -0500 $
+** Khronos $Git commit SHA1: 402ce7f591 $ on $Git commit date: 2024-07-09 16:33:12 +0000 $
 */
 
 #include <EGL/eglplatform.h>
 
-#define EGL_EGLEXT_VERSION 20231215
+#define EGL_EGLEXT_VERSION 20240710
 
 /* Generated C header for:
  * API: egl
@@ -550,6 +550,11 @@ EGLAPI EGLBoolean EGLAPIENTRY eglPresentationTimeANDROID (EGLDisplay dpy, EGLSur
 #define EGL_ANDROID_recordable 1
 #define EGL_RECORDABLE_ANDROID            0x3142
 #endif /* EGL_ANDROID_recordable */
+
+#ifndef EGL_ANDROID_telemetry_hint
+#define EGL_ANDROID_telemetry_hint 1
+#define EGL_TELEMETRY_HINT_ANDROID        0x3570
+#endif /* EGL_ANDROID_telemetry_hint */
 
 #ifndef EGL_ANGLE_d3d_share_handle_client_buffer
 #define EGL_ANGLE_d3d_share_handle_client_buffer 1
